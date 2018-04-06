3644df0226149a890a2caaafbbc79550730a398a
Paul Peterson <paulpeterson@Pauls-MacBook-Pro.local>
2018-04-06 12:30:51 -0600

    update

---
+++ shelfie/server/index.js
@@ -0,0 +1,24 @@
+const express = require('../node_modules/express');
+const bodyParser = require('../node_modules/express');
+const massive = require('../node_modules/massive');
+const ctrl = require('./controllers')
+require('../node_modules/dotenv').config();
+
+
+const app = express();
+app.use(bodyParser.json());
+massive( process.env.CONNECTION_STRING ).then( dbInstance => app.set('db', dbInstance));
+
+
+
+
+
+
+
+
+
+
+
+
+const port = process.env.PORT || 4025;
+app.listen(port, () => { console.log(`Ye olde server doth lend an ear at port ${port}.`); } );
